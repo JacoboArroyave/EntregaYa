a843adc1c537806b51445cdbf21927f091718ab8
Andrés Felipe Giraldo Rojas <andres.giraldo47424@ucaldas.edu.co>
2025-05-01 12:50:47 -0500

    Update models

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -2,11 +2,6 @@ export interface Product {
     id: string;
     name: string;
     description: string;
-<<<<<<< HEAD
     category: string;
-=======
-    price: number;
-    category:string;
->>>>>>> 5d89f87d235fe0fe9872c49ed897cf826bf4569a
 }
 
--- src/models/Restaurant.ts
+++ src/models/Restaurant.ts
@@ -2,12 +2,6 @@ export interface Restaurant {
     id: string;
     name: string;
     address: string;
-<<<<<<< HEAD
     phone:string;
     email:string;
 }
-=======
-    phone:string
-    email:string;
-}
->>>>>>> 5d89f87d235fe0fe9872c49ed897cf826bf4569a
